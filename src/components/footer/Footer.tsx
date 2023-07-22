746c66f01e3a0e7f3e37a5868dc063dd6211f25f
giatha0 <thao8c89@gmail.com>
2023-07-22 17:19:12 +0700

    design navbar menu footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <span>Thao Duong Gia</span>
+            <span>©  Admin DashBoard</span>
+        </div>
+    )
+}
+
+export default Footer

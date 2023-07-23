42727c282fe2f4ae820b919ddb8d0380c4aca67c
giatha0 <thao8c89@gmail.com>
2023-07-23 17:44:01 +0700

    fix a bit

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -30,7 +30,7 @@ const Single = (props: Props) => {
                     <div className="details">
                         {Object.entries(props.info).map(item => (
                             <div className="item" key={item[0]}>
-                                <span className="itemTitle">{item[0]}</span>
+                                <span className="itemTitle">{item[0]}:</span>
                                 <span className="itemValue">{item[1]}</span>
                             </div>
                         ))}
